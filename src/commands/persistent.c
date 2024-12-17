fe7e636da7ed3e9b5723a1552eff3521e9bf40b7
Semih Artan <s-785412369@hotmail.com>
2024-12-17 13:40:21 +0300

    Use new error reporting macros.

--- src/commands/persistent.c
+++ src/commands/persistent.c
@@ -55,14 +55,14 @@ int ProcessCommandPersistent(int argc, const TCHAR* argv[])
 
 	SOS_HALT_IF_NULL(hProcessKillEvent = OpenEvent(EVENT_ALL_ACCESS,
 		FALSE, SOS_PROCESS_KILL_EVENT_NAME),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
+		SOS_REPORT_WIN32_ERROR();
 		SOS_LOG_ERROR("OpenEvent failed: %s.\n", SOS_LAST_ERROR_MESSAGE););
 
 	SOS_HALT_IF_NULL(s_hFileMappingObject = OpenFileMapping(
 		FILE_MAP_ALL_ACCESS,   // read/write access
 		FALSE,                 // do not inherit the name
 		SOS_FILE_MAPPING_OBJECT_NAME),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
+		SOS_REPORT_WIN32_ERROR();
 		SOS_LOG_ERROR("OpenFileMapping failed: %s.\n", SOS_LAST_ERROR_MESSAGE););
 	 
 	SOS_HALT_IF_NULL(s_pSharedMemoryBuffer = MapViewOfFile(
@@ -71,37 +71,37 @@ int ProcessCommandPersistent(int argc, const TCHAR* argv[])
 		0,
 		0,
 		SOS_SHARED_MEMORY_SIZE),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
+		SOS_REPORT_WIN32_ERROR();
 		SOS_LOG_ERROR("MapViewOfFile failed: %s.", SOS_LAST_ERROR_MESSAGE);
 		CloseHandle(s_hFileMappingObject););
 	  
 	SOS_HALT_IF_FAILED(hr = PowerRegisterForEffectivePowerModeNotifications(EFFECTIVE_POWER_MODE_V2, EffectiveModeChangeCallback, NULL, &s_effectiveModeChangeNotificationHandle),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
+		SOS_REPORT_WIN32_ERROR();
 		SOS_LOG_ERROR("PowerRegisterForEffectivePowerModeNotifications failed: %s.",
-		SOS_HR_ERROR_MESSAGE(hr));); 
+		SOS_HRESULT_MESSAGE(hr));); 
 
 	s_persistentSchemeMoniker = (const TCHAR*)s_pSharedMemoryBuffer;
 
 	SOS_HALT_IF_FAILED(hr = SosOverlayScheme_GetActiveScheme(&activeScheme),
-		SOS_REPORT_APP_ERROR();
+		SOS_REPORT_HR_ERROR();
 		DoCleanUp(););
 	s_previousSchemeMoniker = activeScheme->alias;
 	
 	SOS_HALT_IF_FAILED(hr = SosOverlayScheme_SetActiveSchemeByAlias(s_persistentSchemeMoniker),
-		SOS_REPORT_APP_ERROR();
+		SOS_REPORT_HR_ERROR();
 		DoCleanUp(););
 	 
 	s_isPersistentSchemeSet = TRUE;
 
 	SOS_HALT_IF_NOT(WAIT_OBJECT_0 != WaitForSingleObject(hProcessKillEvent, INFINITE),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
+		SOS_REPORT_WIN32_ERROR();
 		SOS_LOG_ERROR("WaitForSingleObject failed: %s.", SOS_LAST_ERROR_MESSAGE);
 		DoCleanUp(););
 	
 	// Restore the previous active overlay scheme.
 	s_isPersistentSchemeSet = FALSE; 
 	SOS_HALT_IF_FAILED(hr = SosOverlayScheme_SetActiveSchemeByAlias(s_previousSchemeMoniker),
-		SOS_REPORT_APP_ERROR();
+		SOS_REPORT_HR_ERROR();
 	DoCleanUp(););
 		
 	DoCleanUp();
@@ -118,18 +118,17 @@ static void __stdcall EffectiveModeChangeCallback(_In_ EFFECTIVE_POWER_MODE _Mod
 	s_persistentSchemeMoniker = (const TCHAR*)s_pSharedMemoryBuffer;
 	//printf("[DEBUG] persistent scheme: %s.\n", persistentSchemeMoniker);
 	SOS_HALT_IF_FAILED(hr = SosOverlayScheme_SetActiveSchemeByAlias(s_persistentSchemeMoniker),
-		SOS_REPORT_APP_ERROR();
-		DoCleanUp();
-	);
+		SOS_REPORT_WIN32_ERROR();
+		DoCleanUp(); );
 }
 
 static void DoCleanUp()
 {
 	HRESULT hr;
 	SOS_HALT_IF_FAILED(hr = PowerUnregisterFromEffectivePowerModeNotifications(s_effectiveModeChangeNotificationHandle),
-		SOS_REPORT_HR_ERROR(SOS_E_WIN32);
-	SOS_LOG_ERROR("PowerUnregisterFromEffectivePowerModeNotifications failed: %s.",
-		SOS_HR_ERROR_MESSAGE(hr)););
+		SOS_REPORT_WIN32_ERROR();
+		SOS_LOG_ERROR("PowerUnregisterFromEffectivePowerModeNotifications failed: %s.", SOS_HRESULT_MESSAGE(hr));
+		);
 	UnmapViewOfFile(s_pSharedMemoryBuffer); 
 	CloseHandle(s_hFileMappingObject);
 }
