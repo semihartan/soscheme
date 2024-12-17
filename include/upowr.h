4f6f53bd4ec5e430549741a394ebc674e5430efd
Semih Artan <s-785412369@hotmail.com>
2024-12-17 13:31:42 +0300

    Correct SAL annotation.

--- include/upowr.h
+++ include/upowr.h
@@ -28,7 +28,7 @@
 #define UPOWR_H
 
 typedef DWORD (WINAPI PowerGetActualOverlayScheme_ft)(_Out_ GUID* SchemeGuid);
-typedef DWORD (WINAPI PowerSetActiveOverlayScheme_ft)(_Out_ const GUID* SchemeGuid);
+typedef DWORD (WINAPI PowerSetActiveOverlayScheme_ft)(_In_ const GUID* SchemeGuid);
 typedef DWORD (WINAPI PowerGetEffectiveOverlayScheme_ft)(_In_ GUID* SchemeGuid);
 typedef DWORD (WINAPI PowerGetOverlaySchemes_ft)(_Out_ GUID** OverlaySchemes, PDWORD lpSchemeCount, BYTE reserved);
 
